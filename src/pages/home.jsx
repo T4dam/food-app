6093780a1b1aa82d3a74f6b9645d2aed1afee93e
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-06-09 19:37:40 +0300

    popular slide is created

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import Veggie from "../components/veggie";
+import Popular from "../components/popular";
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <Veggie />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home
