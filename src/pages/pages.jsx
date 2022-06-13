8cda5056d27970d534c762779e8a403e8499c0f6
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-06-13 12:59:27 +0300

    creating recipe page

--- src/pages/pages.jsx
+++ src/pages/pages.jsx
@@ -3,6 +3,7 @@ import Home from './home';
 import Cuisine from './cuisine'
 import {Route, Routes} from 'react-router-dom';
 import Searched from './searched';
+import Recipe from './recipe';
 
 const Pages = () => {
   return (
@@ -11,6 +12,7 @@ const Pages = () => {
             <Route path='/' element={<Home />} />
             <Route path='/cuisine/:type' element={<Cuisine />} />
             <Route path='/searched/:search' element={<Searched />} />
+            <Route path='/recipe/:name' element={<Recipe />} />
         </Routes>
     
 
