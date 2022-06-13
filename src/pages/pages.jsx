2b291a37fc554807bb2a0dfbac1e33c906824c5f
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-06-13 10:42:59 +0300

    recipe grid

--- src/pages/pages.jsx
+++ src/pages/pages.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Home from './home';
 import Cuisine from './cuisine'
-import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 const Pages = () => {
   return (
   
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/cuisine' element={<Cuisine />} />
+            <Route path='/cuisine/:type' element={<Cuisine />} />
 
         </Routes>
     
