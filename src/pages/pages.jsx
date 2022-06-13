7a50177f48fe3583e8f72094ebae92b8a080f51d
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-06-13 11:45:34 +0300

    search enabled

--- src/pages/pages.jsx
+++ src/pages/pages.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './home';
 import Cuisine from './cuisine'
 import {Route, Routes} from 'react-router-dom';
+import Searched from './searched';
 
 const Pages = () => {
   return (
@@ -9,7 +10,7 @@ const Pages = () => {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/cuisine/:type' element={<Cuisine />} />
-
+            <Route path='/searched/:search' element={<Searched />} />
         </Routes>
     
 
